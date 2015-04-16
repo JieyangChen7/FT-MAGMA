67e5a88ed8ac36f1fe60ec04b15772ba220ddf61
Jieyang <cjy7117@gmail.com>
2015-04-16 11:53:52 -0700

    finished dtrsmFT

--- dtrsmFT.h
+++ dtrsmFT.h
@@ -1,15 +1,17 @@
 //TRSM with FT on GPU using cuBLAS
 
-__global__ detectAndCorrect(double * B, int ldb, 
+__global__ detectAndCorrect(double * B, int ldb, int n,
 		double * chksumB1, int incB1, double * chksumB2, int incB2,
 		double * chkB1, int incB1_2, double * chkB2, int incB2_2){
 	//determin the reponsisble column 
 	int block = blockIdx.x;
 	int col = threadIdx.x;
-	
-	
-	
-	
+	double diff = abs(*(chkB1+block+col*incB1_2)-*(chksumB1+block+col*incB1);
+	if(diff>0.1){
+		double diff2=abs(*(chkB2+block+col*incB2_2)-*(chksumB2+block+col*incB2);
+		int row = (int)round(diff2/diff);
+		*(B+n*block+row+col*ldb) += *(chksumB1+block+col*incB1)-*(chkB1+block+col*incB1_2);
+	}
 }
 
 /*
@@ -79,5 +81,7 @@ void dtrsmFT(cublasHandle_t handle, int m, int n, double * A, int lda, double * 
 				A, lda,
 				checksumB2, incB2);
 	
-	
+	detectAndCorrect<<<dim3(m/n),dim3(n)>>>(B, ldb, n,
+			checksumB1, incB1, checksumB2, incB2,
+			chk1, chk1_ld, chk2, chk2_ld);
 }
