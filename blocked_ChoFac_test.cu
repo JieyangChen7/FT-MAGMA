d5c049576087a2da5a014287aa5f7a4ef67ee99f
Jieyang <cjy7117@gmail.com>
2015-04-25 15:24:32 -0700

    check larger size

--- blocked_ChoFac_test.cu
+++ blocked_ChoFac_test.cu
@@ -229,7 +229,7 @@ void test_mydpotrf(int N, int B, float * real_time, float * proc_time,
 int main(int argc, char**argv) {
 
 	int TEST_NUM = 1;
-	int n[10] = { 16, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240 };
+	int n[10] = { 1024, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240 };
 	int b = 2;
 	for (int k = 0; k < 1; k++) {
 		float total_real_time = 0.0;
