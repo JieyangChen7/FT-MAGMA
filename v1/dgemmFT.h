d20fed620738a5a07cd0aa691650118bab3c2619
Jieyang <cjy7117@gmail.com>
2015-05-14 13:08:40 -0500

    make copy

---
+++ v1/dgemmFT.h
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+//dgemm with FT
+
+__global__ void detectAndCorrectForGemm(double * C, int ldc, int n,
+		double * chksumC1, int incC1, double * chksumC2, int incC2,
+		double * chkC1, int incC1_2, double * chkC2, int incC2_2){
+	//determin the reponsisble column 
+	int block = blockIdx.x;
+	int col = threadIdx.x;
+	double diff = abs(*(chkC1+block+col*incC1_2)-*(chksumC1+block+col*incC1));
+	if(diff>0.1){
+		double diff2=abs(*(chkC2+block+col*incC2_2)-*(chksumC2+block+col*incC2));
+		int row = (int)round(diff2/diff)-1;
+		*(C+n*block+row+col*ldc) += *(chksumC1+block+col*incC1)-*(chkC1+block+col*incC1_2);
+	}
+}
+
+/**
+ * m: number of row of A
+ * n: number of col of B
+ * k: number of col of A / row of B
+ */
+void dgemmFT(cublasHandle_t handle, int m, int n, int k, double * A, int lda,
+		double * B, int ldb, double * C, int ldc, double * checksumA1,
+		int incA1, double * checksumA2, int incA2, double * checksumC1,
+		int incC1, double * checksumC2, int incC2,
+		double * v1d, double * v2d,
+		double * chk1, int chk1_ld, double * chk2, int chk2_ld, bool FT) {
+
+	/*cout<<"checksum1 of A before dgemm:"<<endl;
+	printMatrix_gpu(checksumA1, incA1*sizeof(double), m/n,k);
+	cout<<"checksum2 of A before dgemm:"<<endl;
+	printMatrix_gpu(checksumA2, incA2*sizeof(double), m/n,k);
+	
+	cout<<"checksum1 of C before dgemm:"<<endl;
+	printMatrix_gpu(checksumC1, incC1*sizeof(double), m/n,n);
+	cout<<"checksum2 of C before dgemm:"<<endl;
+	printMatrix_gpu(checksumC2, incC2*sizeof(double), m/n,n);
+	*/
+	double negone = -1;
+	double one = 1;
+	double zero = 0;
+	//cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, m, n, k, &negone, A, lda, B,
+	//		ldb, &one, C, ldc);
+
+	if(FT){
+		
+		//recalculate checksum1 and checksum2
+	/*	for (int i = 0; i < m; i += n) {
+			cublasDgemv(handle, CUBLAS_OP_T, n, n, &one, C + i, ldc, v1d, 1,
+					&zero, chk1 + (i / n), chk1_ld);
+			cublasDgemv(handle, CUBLAS_OP_T, n, n, &one, C + i, ldb, v2d, 1,
+					&zero, chk2 + (i / n), chk2_ld);
+		}
+		*/
+		
+		/*cout<<"recalculated checksum1 of C after dgemm:"<<endl;
+		printMatrix_gpu(chk1, chk1_pitch, m/n,n);
+		cout<<"recalculated checksum2 of C after dgemm:"<<endl;
+		printMatrix_gpu(chk2, chk2_pitch, m/n,n);
+		*/	
+		
+		
+		//update checksum1 and checksum2
+		cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, (m/n)*2, n, k, &negone,
+				checksumA1, incA1, B, ldb, &one, checksumC1, incC1);
+		//cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, m/n, n, k, &negone,
+		//		checksumA2, incA2, B, ldb, &one, checksumC2, incC2);
+		
+		
+		/*cout<<"updated checksum1 of C after dgemm:"<<endl;
+		printMatrix_gpu(checksumC1, incC1*sizeof(double), m/n,n);
+		cout<<"updated checksum2 of C after dgemm:"<<endl;
+		printMatrix_gpu(checksumC2, incC2*sizeof(double), m/n,n);
+		*/
+		//error detection and error correction
+	//	detectAndCorrectForGemm<<<dim3(m/n),dim3(n)>>>(C, ldc, n,
+	//			checksumC1, incC1, checksumC2, incC2,
+	//			chk1, chk1_ld, chk2, chk2_ld);
+				
+		
+	}
+}
---
+++ v1/dpotrfFT.h
@@ -0,0 +1,89 @@
+using namespace std;
+//Cholesky Factorization with FT on CPU using ACML
+double get(double * matrix, int ld, int n, int i, int j) {
+	if (i > ld || j > n)
+		cout << "matrix_get_error" << endl;
+	return *(matrix + j * ld + i);
+}
+/**
+ * Cholesky factorization with FT on CPU using ACML
+ * A: input matrix
+ * lda: leading dimension of A
+ * n: size of A
+ * chksum1: checksum 1
+ * inc1: stride between elememts in chksum1
+ * chksum2: checksum 2
+ * inc2: stride between elememts in chksum2
+ */
+void dpotrfFT(double * A, int lda, int n, 
+		double * chksum1, int inc1, double * chksum2, int inc2, 
+		double * v1, double * v2, bool FT ) {
+	double alpha = 1;
+	double beta = 0;
+	
+	//cout<<"matrix A before dpotrf:"<<endl;
+	//printMatrix_host(A,n,n);
+	
+	//do Choleksy factorization
+	int info;
+	dpotrf('L', n, A, n, &info);
+	
+	if(FT){
+	
+		//cout<<"matrix A after dpotrf:"<<endl;
+		//printMatrix_host(A,n,n);
+		
+		/*cout<<"checksum on CPU before factorization:"<<endl;
+		printVector_host(chksum1, n);
+		printVector_host(chksum2, n);
+		*/
+		//recalculate checksum1 and checksum2
+		
+		double * chk1 = new double[n];
+		double * chk2 = new double[n];
+		dgemv('T', n, n, alpha, A, lda, v1, 1, beta, chk1, 1);
+		dgemv('T', n, n, alpha, A, lda, v2, 1, beta, chk2, 1);
+		
+		//cout<<"recalcuated checksum on CPU after factorization:"<<endl;
+		//printVector_host(chk1, n);
+		//printVector_host(chk2, n);
+		
+		double negone = -1;
+		//update checksum1 and checksum2
+		for (int i = 0; i < n; i++) {
+			chksum1[i] = chksum1[i] / get(A, n, n, i, i);
+			daxpy(n-i-1, negone*chksum1[i], A + i*lda + i+1, 1, chksum1 + i+1, 1 );
+			/*for (int j = i + 1; j < n; j++) {
+				chksum1[j] = chksum1[j] - chksum1[i] * get(A, n, n, j, i);
+			}
+			*/
+		}
+	
+		for (int i = 0; i < n; i++) {
+			chksum2[i] = chksum2[i] / get(A, n, n, i, i);
+			daxpy(n-i-1, negone*chksum2[i], A + i*lda + i+1, 1, chksum2 + i+1, 1 );
+			/*for (int j = i + 1; j < n; j++) {
+				chksum2[j] = chksum2[j] - chksum2[i] * get(A, n, n, j, i);
+			}
+			*/
+		}
+		
+		//cout<<"updated checksum on CPU after factorization:"<<endl;
+		//printVector_host(chksum1, n);
+		//printVector_host(chksum2, n);
+	
+		//checking error to be finished
+		/*for(int i=0;i<n;i++){
+			double diff = abs(chk1[i]-chksum1[i]);
+			if(diff>0.1){//error detected
+				//determine position
+				cout<<"Error detected in dpotrf"<<endl;
+				double diff2 = abs(chk2[i]-chksum2[i]);
+				int j=(int)round(diff2/diff)-1;
+				//correct error
+				*(A+i*lda+j) += chksum1[i] - chk1[i];
+			}
+		}
+		*/
+	}
+}
