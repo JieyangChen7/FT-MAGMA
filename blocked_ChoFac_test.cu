d813df630b0c538103f2b2cd9c8eef36174b863d
Jieyang Chen <cjy7117@gmail.com>
2015-05-18 09:10:29 -0700

    test

--- blocked_ChoFac_test.cu
+++ blocked_ChoFac_test.cu
@@ -145,7 +145,7 @@ void my_dpotrf(char uplo, double * matrix, int ld, int N, int B,
 		 //printMatrix_gpu(matrix, ld*sizeof(double), N, N);
 		 //cout<<"checksum:"<<endl;
 		 //printMatrix_gpu(checksum, checksum_pitch, (N/B)*2, N);
-		/*
+		
 		if (i > 0) {
 			dsyrkFT(handle1, B, i, matrix + i, ld, matrix + i * ld + i, ld,
 					checksum + (i / B)*2, checksum_ld,
@@ -155,7 +155,7 @@ void my_dpotrf(char uplo, double * matrix, int ld, int N, int B,
 					FT);
 			
 		}
-		
+		/*
 		
 		cudaStreamSynchronize(stream1);
 		
@@ -209,14 +209,15 @@ void my_dpotrf(char uplo, double * matrix, int ld, int N, int B,
 		*/
 		
 		//update B    
-		
+		/*
 		if (i + B < N) {
-		//	cudaStreamSynchronize(stream0);
+			cudaStreamSynchronize(stream0);
 			dtrsmFT(handle1, N - i - B, B, matrix + i * ld + i, ld,
 					matrix + i * ld + i + B, ld,
 					checksum + ((i + B) / B )*2 + i * checksum_ld, checksum_ld,
 					vd, vd_ld, chkd, chkd_ld, FT);
 		}
+		*/
 		
 		
 
