d7b18601d97eee8c26e793eb4795fe789812db31
Jieyang <cjy7117@gmail.com>
2015-05-21 12:11:08 -0700

    test

--- v2/blocked_ChoFac_test.cu
+++ v2/blocked_ChoFac_test.cu
@@ -245,18 +245,18 @@ void test_mydpotrf(int N, int B, float * real_time, float * proc_time,
 
 	char uplo = 'l';
 	double * matrix;
-	//double * result;
+	double * result;
 	size_t matrix_pitch;
-	//size_t result_pitch;
+	size_t result_pitch;
 	//Memory allocation on RAM and DRAM
 	cudaMallocPitch((void**) &matrix, &matrix_pitch, N * sizeof(double), N);
-	//cudaMallocPitch((void**) &result, &result_pitch, N * sizeof(double), N);
+	cudaMallocPitch((void**) &result, &result_pitch, N * sizeof(double), N);
 
 	int matrix_ld = matrix_pitch / sizeof(double);
 	//int result_ld = result_pitch / sizeof(double);
 
-	//matrixGenerator_gpu2(uplo, matrix, matrix_ld, result, result_ld, N, 2);
-	matrixGenerator_gpu(uplo, matrix, matrix_ld, N, 2);
+	matrixGenerator_gpu2(uplo, matrix, matrix_ld, result, result_ld, N, 2);
+	//matrixGenerator_gpu(uplo, matrix, matrix_ld, N, 2);
 
 	my_dpotrf(uplo, matrix, matrix_ld, N, B, real_time, proc_time, flpins, \
 			mflops, FT, DEBUG);
@@ -269,7 +269,7 @@ void test_mydpotrf(int N, int B, float * real_time, float * proc_time,
 	 }
 	 */
 	cudaFree(matrix);
-	//cudaFree(result);
+	cudaFree(result);
 
 }
 
