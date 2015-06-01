a742eabbd718fcf937c411ff3dcfe8aeb56fc334
Jieyang Chen <cjy7117@gmail.com>
2015-06-01 15:23:28 -0700

    test

--- checksumGenerator.h
+++ checksumGenerator.h
@@ -4,10 +4,10 @@ double * initializeChecksum(cublasHandle_t handle, double * matrix, int ld, int 
 		double * vd, int vd_ld) {
 
 	//cout<<"checksum vector on GPU:"<<endl;
-	size_t checksum_dev_pitch
+	size_t checksum_dev_pitch;
 	double * checksum_dev;
-	cudaMallocPitch((void**) &chksum_dev, &chksum_dev_pitch, (N / B) * 2 * sizeof(double), N);
-	int checksum_dev_ld = chksum_pitch / sizeof(double);
+	cudaMallocPitch((void**) &checksum_dev, &checksum_dev_pitch, (N / B) * 2 * sizeof(double), N);
+	int checksum_dev_ld = checksum_dev_pitch / sizeof(double);
 	
 	
 	//printMatrix_gpu(matrix,ld*sizeof(double),N,N);
@@ -16,7 +16,7 @@ double * initializeChecksum(cublasHandle_t handle, double * matrix, int ld, int 
 	double zero = 0;
 	for (int i = 0; i < N; i += B) {
 		cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, 2, N, B, &one, vd, vd_ld, matrix + i, ld,
-				&zero, chksum + (i / B) * 2, chksum_ld);
+				&zero, checksum_dev + (i / B) * 2, checksum_dev_ld);
 	}
 	
 	double * checksum_host;
