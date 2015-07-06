17243e7481a6024e8ad699541a6129f256c2075e
Jieyang <cjy7117@gmail.com>
2015-07-05 18:09:07 -0700

    test

--- magma-1.6.2-v1/magmablas/dsyrkFT.cpp
+++ magma-1.6.2-v1/magmablas/dsyrkFT.cpp
@@ -43,7 +43,7 @@ void dsyrkFT(int n, int m, double * A, int lda, double * C, int ldc,
 	double zero = 0;
 	//cublasDsyrk(handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, n, m, &negone, A, lda, &one, C, ldc);
 	
-	
+	printMatrix_gpu(C, ldc, n, n);
 	magma_dgemm(
 			MagmaNoTrans, MagmaTrans,
 			n, n, m,
