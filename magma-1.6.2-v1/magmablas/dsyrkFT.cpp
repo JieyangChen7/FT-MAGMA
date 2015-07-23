142dd8f883790cdfb0655cf75e59fd00afea446a
Jieyang <cjy7117@gmail.com>
2015-07-23 14:08:51 -0700

    test

--- magma-1.6.2-v1/magmablas/dsyrkFT.cpp
+++ magma-1.6.2-v1/magmablas/dsyrkFT.cpp
@@ -56,10 +56,10 @@ void dsyrkFT(cublasHandle_t handle, int n, int m, double * A, int lda, double * 
 	if(FT){
 		
 		//recalculate checksum1 and checksum2
-//		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-//				C, ldc, v1d, 1, MAGMA_D_ZERO, chk1, chk1_ld );
-//		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-//				C, ldc, v2d, 1, MAGMA_D_ZERO, chk2, chk2_ld );
+		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+				C, ldc, v1d, 1, MAGMA_D_ZERO, chk1, chk1_ld );
+		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+				C, ldc, v2d, 1, MAGMA_D_ZERO, chk2, chk2_ld );
 		
 //		cublasDgemv(handle, CUBLAS_OP_T, n, n, &one, C, ldc, v1d, 1, &zero, chk1, chk1_ld);
 //		cublasDgemv(handle, CUBLAS_OP_T, n, n, &one, C, ldc, v2d, 1, &zero, chk2, chk2_ld);
