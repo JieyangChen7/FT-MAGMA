3b3fc1bd7b96fcef363a74942e9e056511f8bd15
Jieyang <cjy7117@gmail.com>
2015-07-27 13:10:13 -0700

    test

--- magma-1.6.2-v3/src/dpotrf_gpu.cpp
+++ magma-1.6.2-v3/src/dpotrf_gpu.cpp
@@ -118,7 +118,7 @@ magma_dpotrf_gpu(
     nb = magma_get_dpotrf_nb(n);
 
     //** debug **//
-        nb = 2;
+    //    nb = 2;
         
         
     if (MAGMA_SUCCESS != magma_dmalloc_pinned( &work, nb*nb )) {
