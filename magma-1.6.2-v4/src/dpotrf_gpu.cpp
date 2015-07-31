b1106f98b6b67cd95bd9ae26cea59e2c17de6bb1
Jieyang <cjy7117@gmail.com>
2015-07-31 14:55:01 -0700

    test

--- magma-1.6.2-v4/src/dpotrf_gpu.cpp
+++ magma-1.6.2-v4/src/dpotrf_gpu.cpp
@@ -338,58 +338,58 @@ magma_dpotrf_gpu(
 							chkd_updateC, chkd_updateC_ld, stream[0],
 							FT, DEBUG);
                 }
-//                              
-//                magma_queue_sync( stream[1] );
-//                magma_dgetmatrix_async( jb, jb,
-//                                        dA(j, j), ldda,
-//                                        work,     jb, stream[0] );
-//                           
-//                if ( (j+jb) < n && j > 0) {
-//					if (FT) {
-//						magma_dgetmatrix_async( jb, j + jb,
-//												dA(j + jb, 0), ldda,
-//												temp, temp_ld,
-//												stream[0] );
-//					}
-//                	dgemmFT((n-j-jb), jb, j, dA(j+jb, 0), ldda,
-//                			dA(j,    0), ldda, dA(j+jb, j), ldda, 
-//                			checksum + ((j + jb) / jb) * 2, checksum_ld, 
-//                			checksum + j * checksum_ld + ((j + jb) / jb) * 2, checksum_ld,
-//                			vd, vd_ld,
-//                			chk1d, chk1d_ld,
-//                			chk2d, chk2d_ld,
-//                			temp, temp_ld,
-//                			stream[0],
-//                			FT, DEBUG);
-//                }
-//
-//                magma_queue_sync( stream[0] );
-//                
-//                dpotrfFT(work, B, B, info, 
-//                		checksum + (j / B) * 2 + j * checksum_ld, checksum_ld, 
-//                		v, v_ld, 
-//                		FT, DEBUG);
-//                
-//                
-//                magma_dsetmatrix_async( jb, jb,
-//                                        work,     jb,
-//                                        dA(j, j), ldda, stream[1] );
-//                
-//                if (*info != 0) {
-//                    *info = *info + j;
-//                    break;
-//                }
+                              
+                magma_queue_sync( stream[1] );
+                magma_dgetmatrix_async( jb, jb,
+                                        dA(j, j), ldda,
+                                        work,     jb, stream[0] );
+                           
+                if ( (j+jb) < n && j > 0) {
+					if (FT) {
+						magma_dgetmatrix_async( jb, j + jb,
+												dA(j + jb, 0), ldda,
+												temp, temp_ld,
+												stream[0] );
+					}
+                	dgemmFT((n-j-jb), jb, j, dA(j+jb, 0), ldda,
+                			dA(j,    0), ldda, dA(j+jb, j), ldda, 
+                			checksum + ((j + jb) / jb) * 2, checksum_ld, 
+                			checksum + j * checksum_ld + ((j + jb) / jb) * 2, checksum_ld,
+                			vd, vd_ld,
+                			chk1d, chk1d_ld,
+                			chk2d, chk2d_ld,
+                			temp, temp_ld,
+                			stream[0],
+                			FT, DEBUG);
+                }
+
+                magma_queue_sync( stream[0] );
                 
-//                if ( (j+jb) < n) {          	
-//                	dtrsmFT((n-j-jb), jb, dA(j,    j), ldda,
-//                			dA(j+jb, j), ldda,
-//                			checksum + ((j + jb) / jb) * 2 + j * checksum_ld, checksum_ld,
-//                			vd, vd_ld, 
-//                			chk1d, chk1d_ld,
-//                			chk2d, chk2d_ld,
-//                			work, jb, 
-//                			FT, DEBUG);
-//                }
+                dpotrfFT(work, B, B, info, 
+                		checksum + (j / B) * 2 + j * checksum_ld, checksum_ld, 
+                		v, v_ld, 
+                		FT, DEBUG);
+                
+                
+                magma_dsetmatrix_async( jb, jb,
+                                        work,     jb,
+                                        dA(j, j), ldda, stream[1] );
+                
+                if (*info != 0) {
+                    *info = *info + j;
+                    break;
+                }
+                
+                if ( (j+jb) < n) {          	
+                	dtrsmFT((n-j-jb), jb, dA(j,    j), ldda,
+                			dA(j+jb, j), ldda,
+                			checksum + ((j + jb) / jb) * 2 + j * checksum_ld, checksum_ld,
+                			vd, vd_ld, 
+                			chk1d, chk1d_ld,
+                			chk2d, chk2d_ld,
+                			work, jb, 
+                			FT, DEBUG);
+                }
                 
 
                 
