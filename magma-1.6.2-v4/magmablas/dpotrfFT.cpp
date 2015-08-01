64eacf275f0de1d67ee268066df0080faf92fc9e
Jieyang <cjy7117@gmail.com>
2015-08-01 16:57:25 -0700

    test

--- magma-1.6.2-v4/magmablas/dpotrfFT.cpp
+++ magma-1.6.2-v4/magmablas/dpotrfFT.cpp
@@ -34,7 +34,7 @@ void dpotrfFT(double * A, int lda, int n, int * info,
 	//int info;
 	//dpotrf('L', n, A, n, &info);
 	char uplo = 'L';
-//	lapackf77_dpotrf(&uplo, &n, A, &n, info);
+	lapackf77_dpotrf(&uplo, &n, A, &n, info);
 	if (FT) {
 	
 		//cout<<"matrix A after dpotrf:"<<endl;
@@ -55,7 +55,7 @@ void dpotrfFT(double * A, int lda, int n, int * info,
 		char trans = 'T';
 		char Ntrans = 'N';
 		int nOfChecksum = 2;
-//		
+		
 		blasf77_dgemv(&trans, &n, &n, &one, A, &lda, v, &v1_inc, &zero, chk1, &chk1_inc );
 		blasf77_dgemv(&trans, &n, &n, &one, A, &lda, v + v_ld, &v2_inc, &zero, chk2, &chk2_inc );
 		
