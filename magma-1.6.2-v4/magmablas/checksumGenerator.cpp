9e582a02c6f58522e80fc4002a9345e3c4082619
Jieyang <cjy7117@gmail.com>
2015-08-01 15:57:11 -0700

    test

--- magma-1.6.2-v4/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-v4/magmablas/checksumGenerator.cpp
@@ -20,16 +20,17 @@ void initializeChecksum(double * matrix, int ld, int N, int B, double * vd, int 
 //					matrix + i, ld,
 //					MAGMA_D_ZERO, chksum + (i / B) * 2, chksum_ld);
 		magma_dsetmatrix_async( 2, B,
+								v, v_ld,
 								chksum + (i / B) * 2 + i * chksum_ld,     chksum_ld,
-								v, v_ld, stream);
+								stream);
 		magma_queue_sync( stream );
 		
-//		magma_dtrmm(
-//			MagmaRight, MagmaLower, MagmaNoTrans, MagmaNonUnit,
-//		    2, B,
-//		    MAGMA_D_ONE,
-//		    matrix + i * ld + i, ld,
-//		    chksum + (i / B) * 2 + i * chksum_ld, chksum_ld );
+		magma_dtrmm(
+			MagmaRight, MagmaLower, MagmaNoTrans, MagmaNonUnit,
+		    2, B,
+		    MAGMA_D_ONE,
+		    matrix + i * ld + i, ld,
+		    chksum + (i / B) * 2 + i * chksum_ld, chksum_ld );
 		
 		//cublasDgemv(handle, CUBLAS_OP_T, B, N, &alpha, matrix + i, ld, vd, 1, \
 				&beta, chksum + (i / B), chksum_ld);
