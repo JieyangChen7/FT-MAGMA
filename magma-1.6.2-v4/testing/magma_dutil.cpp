29ff61c5eed03f9ff099a99bb24581d17635edf5
Jieyang <cjy7117@gmail.com>
2015-08-02 14:46:49 -0700

    test

--- magma-1.6.2-v4/testing/magma_dutil.cpp
+++ magma-1.6.2-v4/testing/magma_dutil.cpp
@@ -42,9 +42,10 @@ void magma_dmake_hpd( magma_int_t N, double* A, magma_int_t lda )
 {
     magma_int_t i, j;
     for( i=0; i<N; ++i ) {
-        A(i,i) = MAGMA_D_MAKE( MAGMA_D_REAL( A(i,i) ) + N, 0. );
+        A(i,i) = (int)MAGMA_D_MAKE( MAGMA_D_REAL( A(i,i) ) + N, 0. );
         for( j=0; j<i; ++j ) {
-            A(j,i) = MAGMA_D_CNJG( A(i,j) );
+            A(i,j) = (int)A(i,j)
+            A(j,i) = (int)MAGMA_D_CNJG( A(i,j) );
         }
     }
 }
