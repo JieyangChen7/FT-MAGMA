a3fa1189eb4abc188b77f065efc04d34dd0b2b75
Jieyang <cjy7117@gmail.com>
2015-08-02 17:14:19 -0700

    test

--- magma-1.6.2-v4/src/dpotrf_gpu.cpp
+++ magma-1.6.2-v4/src/dpotrf_gpu.cpp
@@ -324,7 +324,7 @@ magma_dpotrf_gpu(
             }
         }
         else {
-        	magma_set_lapack_numthreads(8);
+        	magma_set_lapack_numthreads(1);
         	int numOfCore = magma_get_lapack_numthreads();
         	cout<<"number of core=" << numOfCore<<endl;
 
