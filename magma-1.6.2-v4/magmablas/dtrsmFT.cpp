c674c66a8d4b7f2cf1b0eecf88a966d221ab7e8a
Jieyang <cjy7117@gmail.com>
2015-08-02 17:44:24 -0700

    test

--- magma-1.6.2-v4/magmablas/dtrsmFT.cpp
+++ magma-1.6.2-v4/magmablas/dtrsmFT.cpp
@@ -39,28 +39,28 @@ void dtrsmFT(int m, int n, double * A, int lda,
 	double negone = -1;
 	double one = 1;
 	double zero = 0;
-//	magma_dtrsm(MagmaRight, MagmaLower, MagmaTrans, MagmaNonUnit,
-//	                                m, n,
-//	                                MAGMA_D_ONE, A, lda,
-//	                                       B, ldb);
+	magma_dtrsm(MagmaRight, MagmaLower, MagmaTrans, MagmaNonUnit,
+	                                m, n,
+	                                MAGMA_D_ONE, A, lda,
+	                                       B, ldb);
 	if (FT) {
 		//recalculate checksums on GPU
 		double beta = 0;
-//		for (int i = 0; i < m; i += n) {
-//			magmablasSetKernelStream(stream2);
-//			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-//					B + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
-//			magmablasSetKernelStream(stream3);
-//			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-//					B + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );			
-//			
-////			magma_dgemm( MagmaNoTrans, MagmaNoTrans,
-////						 2, n, n,
-////						 MAGMA_D_ONE, vd, vd_ld,
-////									B + i, ldb,
-////						 MAGMA_D_ZERO, chk1 + (i/n)*2, chk1_ld);
-//		}
-//		magmablasSetKernelStream(stream1);
+		for (int i = 0; i < m; i += n) {
+			magmablasSetKernelStream(stream2);
+			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+					B + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
+			magmablasSetKernelStream(stream3);
+			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+					B + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );			
+			
+//			magma_dgemm( MagmaNoTrans, MagmaNoTrans,
+//						 2, n, n,
+//						 MAGMA_D_ONE, vd, vd_ld,
+//									B + i, ldb,
+//						 MAGMA_D_ZERO, chk1 + (i/n)*2, chk1_ld);
+		}
+		magmablasSetKernelStream(stream1);
 		
 		//update checksums on CPU
 		char R = 'R';
