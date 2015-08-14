dae5da9077d761ed2de6c906179551ca9e61aa0a
Jieyang <cjy7117@gmail.com>
2015-08-13 20:31:56 -0700

    test

--- magma-1.6.2-v6/magmablas/dgemmFT.cpp
+++ magma-1.6.2-v6/magmablas/dgemmFT.cpp
@@ -77,18 +77,18 @@ void dgemmFT(int m, int n, int k, double * A, int lda,
 		
 		
 		//do part of A verify on CPU
-//		double r = 0.8;
-//		double * temp_cpu;
-//		int temp_cpu_ld;
-//		int cpu_start_index = (int)((m / n) * r) * n;
-//		if (cpu_start_index < m) {
-//			magma_dmalloc_pinned(&temp_cpu, (m - cpu_start_index) * k * sizeof(double));
-//			temp_cpu_ld = m - cpu_start_index;
-//			magma_dgetmatrix_async(m - cpu_start_index, k,
-//									A + cpu_start_index, lda,
-//									temp_cpu, temp_cpu_ld,
-//									stream0);
-//		}
+		double r = 0.8;
+		double * temp_cpu;
+		int temp_cpu_ld;
+		int cpu_start_index = (int)((m / n) * r) * n;
+		if (cpu_start_index < m) {
+			magma_dmalloc_pinned(&temp_cpu, (m - cpu_start_index) * k * sizeof(double));
+			temp_cpu_ld = m - cpu_start_index;
+			magma_dgetmatrix_async(m - cpu_start_index, k,
+									A + cpu_start_index, lda,
+									temp_cpu, temp_cpu_ld,
+									stream0);
+		}
 //		//verify A before use
 //		for (int i = 0; i < m; i += n) {
 //			magmablasSetKernelStream(stream2);
