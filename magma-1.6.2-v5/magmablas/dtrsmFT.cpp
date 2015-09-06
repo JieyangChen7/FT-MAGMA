71ef16dcd606605deafe698895fa4358cfb1840d
Jieyang <cjy7117@gmail.com>
2015-09-06 13:45:18 -0700

    test

--- magma-1.6.2-v5/magmablas/dtrsmFT.cpp
+++ magma-1.6.2-v5/magmablas/dtrsmFT.cpp
@@ -48,10 +48,10 @@ void dtrsmFT(int m, int n, double * A, int lda,
 //		magma_queue_sync( stream1 );
 		double beta = 0;
 		for (int i = 0; i < m; i += n) {
-//			magmablasSetKernelStream(stream2);
+			magmablasSetKernelStream(stream2);
 			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
 					B + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
-//			magmablasSetKernelStream(stream3);
+			magmablasSetKernelStream(stream3);
 			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
 					B + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );			
 		}
