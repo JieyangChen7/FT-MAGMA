be9a5c05fbcfe8529837e2d33a2d0ac09de47339
Jieyang Chen <cjy7117@gmail.com>
2015-09-07 00:14:59 -0700

    test

--- magma-1.6.2-v5/magmablas/dpotrfFT.cpp
+++ magma-1.6.2-v5/magmablas/dpotrfFT.cpp
@@ -46,16 +46,16 @@ void dpotrfFT(double * A, int lda, int n, int * info,
 		*/
 		
 		//recalculate checksum1 and checksum2
-//		double * chk1 = new double[n];
-//		double * chk2 = new double[n];
-//		for (int i = 0; i < n; i++) {
-//			chk1[i] = 1;
-//			chk2[i] = i + 1;
-//		}
-//		int v1_inc = 1;
-//		int v2_inc = 1;
-//		int chk1_inc = 1;
-//		int chk2_inc = 1;
+		double * chk1 = new double[n];
+		double * chk2 = new double[n];
+		for (int i = 0; i < n; i++) {
+			chk1[i] = 1;
+			chk2[i] = i + 1;
+		}
+		int v1_inc = 1;
+		int v2_inc = 1;
+		int chk1_inc = 1;
+		int chk2_inc = 1;
 		char T = 'T';
 		char N = 'N';
 		char L = 'L';
