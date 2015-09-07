3999f31069f54c067112d0e859358c6b5281485c
Jieyang <cjy7117@gmail.com>
2015-09-06 20:19:43 -0700

    test

--- magma-1.6.2-v5/magmablas/dsyrkFT.cpp
+++ magma-1.6.2-v5/magmablas/dsyrkFT.cpp
@@ -50,64 +50,64 @@ void dsyrkFT(int n, int m, double * A, int lda, double * C, int ldc,
 
 	
 	
-	if (FT) {
-		magma_dgemm(
-				MagmaNoTrans, MagmaTrans,
-				n, n, m,
-				MAGMA_D_ONE * (-1),
-				A, lda, A, lda,
-				MAGMA_D_ONE,
-				C, ldc );
-	} else {
-		magma_dsyrk(MagmaLower, MagmaNoTrans, n, m,
-						MAGMA_D_ONE * (-1), A, lda,
-						MAGMA_D_ONE,     C, ldc);
-	}
+//	if (FT) {
+//		magma_dgemm(
+//				MagmaNoTrans, MagmaTrans,
+//				n, n, m,
+//				MAGMA_D_ONE * (-1),
+//				A, lda, A, lda,
+//				MAGMA_D_ONE,
+//				C, ldc );
+//	} else {
+//		magma_dsyrk(MagmaLower, MagmaNoTrans, n, m,
+//						MAGMA_D_ONE * (-1), A, lda,
+//						MAGMA_D_ONE,     C, ldc);
+//	}
 
 	
 	if(FT){
-		//update checksums on GPU
-		magmablasSetKernelStream(stream0);
-		magma_dgemm(
-					MagmaNoTrans, MagmaTrans,
-					2, n, m,
-					MAGMA_D_ONE * (-1),
-					chkd_updateA, chkd_updateA_ld, A, lda,
-					MAGMA_D_ONE,
-					chkd_updateC, chkd_updateC_ld );
-		
-		//transfer updated checksum back to CPU
-		magma_dgetmatrix_async( 2, n,
-								chkd_updateC, chkd_updateC_ld,
-								checksumC, checksumC_ld, stream0);
-		
-		//recalculate checksum1 and checksum2
-//		magma_queue_sync( stream1 );
-		magmablasSetKernelStream(stream2);
-		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-				C, ldc, vd, vd_ld, MAGMA_D_ZERO, chk1, chk1_ld );
-		magmablasSetKernelStream(stream3);
-		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
-				C, ldc, vd + 1, vd_ld, MAGMA_D_ZERO, chk2, chk2_ld );
-		magmablasSetKernelStream(stream1);
-		
-		
-//		//update checksum1 and checksum2
-//		char N = 'N';
-//		char T = 'T';
-//		int m2 = 2;
-//		int n2 = n;
-//		int k2 = m;
+//		//update checksums on GPU
+//		magmablasSetKernelStream(stream0);
+//		magma_dgemm(
+//					MagmaNoTrans, MagmaTrans,
+//					2, n, m,
+//					MAGMA_D_ONE * (-1),
+//					chkd_updateA, chkd_updateA_ld, A, lda,
+//					MAGMA_D_ONE,
+//					chkd_updateC, chkd_updateC_ld );
+//		
+//		//transfer updated checksum back to CPU
+//		magma_dgetmatrix_async( 2, n,
+//								chkd_updateC, chkd_updateC_ld,
+//								checksumC, checksumC_ld, stream0);
+//		
+//		//recalculate checksum1 and checksum2
+////		magma_queue_sync( stream1 );
+//		magmablasSetKernelStream(stream2);
+//		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+//				C, ldc, vd, vd_ld, MAGMA_D_ZERO, chk1, chk1_ld );
+//		magmablasSetKernelStream(stream3);
+//		magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
+//				C, ldc, vd + 1, vd_ld, MAGMA_D_ZERO, chk2, chk2_ld );
+//		magmablasSetKernelStream(stream1);
 //		
 //		
-//		blasf77_dgemm(  &N, &T,
-//		                &m2, &n2, &k2,
-//		                &negone,
-//		                checksumA, &checksumA_ld,
-//		                temp, &temp_ld,
-//		                &one,
-//		                checksumC, &checksumC_ld );
-//		 
+////		//update checksum1 and checksum2
+////		char N = 'N';
+////		char T = 'T';
+////		int m2 = 2;
+////		int n2 = n;
+////		int k2 = m;
+////		
+////		
+////		blasf77_dgemm(  &N, &T,
+////		                &m2, &n2, &k2,
+////		                &negone,
+////		                checksumA, &checksumA_ld,
+////		                temp, &temp_ld,
+////		                &one,
+////		                checksumC, &checksumC_ld );
+////		 
 		
 		if (DEBUG) {
 			
