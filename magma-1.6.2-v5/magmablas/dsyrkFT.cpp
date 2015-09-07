5c63b54ffa991690993303d9a55d9b3f2a4967cc
Jieyang <cjy7117@gmail.com>
2015-09-06 21:08:16 -0700

    test

--- magma-1.6.2-v5/magmablas/dsyrkFT.cpp
+++ magma-1.6.2-v5/magmablas/dsyrkFT.cpp
@@ -50,32 +50,32 @@ void dsyrkFT(int n, int m, double * A, int lda, double * C, int ldc,
 
 	
 	
-	if (FT) {
-		magma_dgemm(
-				MagmaNoTrans, MagmaTrans,
-				n, n, m,
-				MAGMA_D_ONE * (-1),
-				A, lda, A, lda,
-				MAGMA_D_ONE,
-				C, ldc );
-	} else {
-		magma_dsyrk(MagmaLower, MagmaNoTrans, n, m,
-						MAGMA_D_ONE * (-1), A, lda,
-						MAGMA_D_ONE,     C, ldc);
-	}
+//	if (FT) {
+//		magma_dgemm(
+//				MagmaNoTrans, MagmaTrans,
+//				n, n, m,
+//				MAGMA_D_ONE * (-1),
+//				A, lda, A, lda,
+//				MAGMA_D_ONE,
+//				C, ldc );
+//	} else {
+//		magma_dsyrk(MagmaLower, MagmaNoTrans, n, m,
+//						MAGMA_D_ONE * (-1), A, lda,
+//						MAGMA_D_ONE,     C, ldc);
+//	}
 
 	
 	if(FT){
-//		//update checksums on GPU
+		//update checksums on GPU
 //		magmablasSetKernelStream(stream0);
-//		magma_dgemm(
-//					MagmaNoTrans, MagmaTrans,
-//					2, n, m,
-//					MAGMA_D_ONE * (-1),
-//					chkd_updateA, chkd_updateA_ld, A, lda,
-//					MAGMA_D_ONE,
-//					chkd_updateC, chkd_updateC_ld );
-//		
+		magma_dgemm(
+					MagmaNoTrans, MagmaTrans,
+					2, n, m,
+					MAGMA_D_ONE * (-1),
+					chkd_updateA, chkd_updateA_ld, A, lda,
+					MAGMA_D_ONE,
+					chkd_updateC, chkd_updateC_ld );
+		
 //		//transfer updated checksum back to CPU
 //		magma_dgetmatrix_async( 2, n,
 //								chkd_updateC, chkd_updateC_ld,
