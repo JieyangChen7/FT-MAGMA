1b0c3c65d976fce526c01cf3c8aadfa3c2934561
Jieyang Chen <cjy7117@gmail.com>
2015-09-15 10:05:25 -0700

    tes

--- magma-1.6.2-v5/magmablas/dsyrkFT.cpp
+++ magma-1.6.2-v5/magmablas/dsyrkFT.cpp
@@ -69,13 +69,13 @@ void dsyrkFT(int n, int m, double * A, int lda, double * C, int ldc,
 	if(FT){
 		//update checksums on GPU
 	//	magmablasSetKernelStream(streams[0]);
-//		magma_dgemm(
-//					MagmaNoTrans, MagmaTrans,
-//					2, n, m,
-//					MAGMA_D_ONE * (-1),
-//					chkd_updateA, chkd_updateA_ld, A, lda,
-//					MAGMA_D_ONE,
-//					chkd_updateC, chkd_updateC_ld );
+		magma_dgemm(
+					MagmaNoTrans, MagmaTrans,
+					2, n, m,
+					MAGMA_D_ONE * (-1),
+					chkd_updateA, chkd_updateA_ld, A, lda,
+					MAGMA_D_ONE,
+					chkd_updateC, chkd_updateC_ld );
 		
 		//transfer updated checksum back to CPU
 //		magma_dgetmatrix_async( 2, n,
