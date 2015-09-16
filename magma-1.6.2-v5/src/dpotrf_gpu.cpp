a957bf2f6150213404eef78460624505e920645a
Jieyang Chen <cjy7117@gmail.com>
2015-09-15 23:45:24 -0700

    test

--- magma-1.6.2-v5/src/dpotrf_gpu.cpp
+++ magma-1.6.2-v5/src/dpotrf_gpu.cpp
@@ -371,7 +371,7 @@ magma_dpotrf_gpu(
                 }
                 
                 int total = (n - j - jb) / jb;
-				double k = 0.3;
+				double k = 0.4;
 				int g_part = (int)(total * k);
 				int c_part = total - g_part;
                 
