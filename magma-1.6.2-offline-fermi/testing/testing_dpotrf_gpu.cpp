63818682172405c4ba43b6a9540e2f89c77b799c
Jieyang Chen <cjy7117@gmail.com>
2015-10-08 22:12:57 -0700

    test

--- magma-1.6.2-offline-fermi/testing/testing_dpotrf_gpu.cpp
+++ magma-1.6.2-offline-fermi/testing/testing_dpotrf_gpu.cpp
@@ -42,7 +42,7 @@ int main( int argc, char** argv)
     
     double tol = opts.tolerance * lapackf77_dlamch("E");
     
-    int Nsize[] = {20480, 17920, 15360, 12800, 10240, 7680, 5120, 16};
+    int Nsize[] = {5120, 7680, 10240, 12800, 15360, 17920, 20480, 16};
     printf("uplo = %s\n", lapack_uplo_const(opts.uplo) );
     printf("  N     CPU GFlop/s (sec)   GPU GFlop/s (sec)   ||R_magma - R_lapack||_F / ||R_lapack||_F\n");
     printf("========================================================\n");
