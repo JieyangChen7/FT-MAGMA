57124c926dcb3379a87ef5a25f824ecfe7499afc
Jieyang <cjy7117@gmail.com>
2015-10-10 16:05:12 -0700

    test

--- magma-1.6.2-partial-kepler/src/dpotrf_gpu.cpp
+++ magma-1.6.2-partial-kepler/src/dpotrf_gpu.cpp
@@ -313,7 +313,7 @@ magma_dpotrf_gpu(
                 //  Update and factorize the current diagonal block and test
                 //  for non-positive-definiteness. Computing MIN
                 //jb = min(nb, (n-j));
-            	int k = 5;
+            	int k = 1;
             	bool VERIFY = false;
             	if (j % k == 0) {
             		VERIFY = true;
