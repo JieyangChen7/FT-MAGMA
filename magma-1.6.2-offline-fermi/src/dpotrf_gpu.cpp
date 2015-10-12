ca0eb6cd007a54db130fc2146f726cc3a3b3fda1
Jieyang Chen <cjy7117@gmail.com>
2015-10-12 07:40:35 -0700

    test

--- magma-1.6.2-offline-fermi/src/dpotrf_gpu.cpp
+++ magma-1.6.2-offline-fermi/src/dpotrf_gpu.cpp
@@ -190,16 +190,7 @@ magma_dpotrf_gpu(
 	int chkd_updateC_ld;
 	
 	
-	float real_time = 0.0;
-				float proc_time = 0.0;
-				long long flpins = 0.0;
-				float mflops = 0.0;
-				//timing start***************
-				if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
-					cout << "PAPI ERROR" << endl;
-					return -1;
-				}
-
+	
 	if (FT) {
 		//cout<<"check sum initialization started"<<endl;
 		//intialize checksum vector on CPU
@@ -336,7 +327,16 @@ magma_dpotrf_gpu(
         else {
         	float noFTtime = 0;
         	float FTtime = 0;
-        	
+        	float real_time = 0.0;
+        					float proc_time = 0.0;
+        					long long flpins = 0.0;
+        					float mflops = 0.0;
+        					//timing start***************
+        					if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+        						cout << "PAPI ERROR" << endl;
+        						return -1;
+        					}
+
 //        	magma_set_lapack_numthreads(64);
 //        	int numOfCore = magma_get_lapack_numthreads();
 //        	cout<<"number of core=" << numOfCore<<endl;
