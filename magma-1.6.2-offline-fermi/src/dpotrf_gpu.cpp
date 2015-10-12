28771b96380df88ab84f2905bf9366399d666eb6
Jieyang Chen <cjy7117@gmail.com>
2015-10-11 22:33:46 -0700

    test

--- magma-1.6.2-offline-fermi/src/dpotrf_gpu.cpp
+++ magma-1.6.2-offline-fermi/src/dpotrf_gpu.cpp
@@ -188,6 +188,17 @@ magma_dpotrf_gpu(
 	
 	double * chkd_updateC;
 	int chkd_updateC_ld;
+	
+	
+	float real_time = 0.0;
+				float proc_time = 0.0;
+				long long flpins = 0.0;
+				float mflops = 0.0;
+				//timing start***************
+				if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+					cout << "PAPI ERROR" << endl;
+					return -1;
+				}
 
 	if (FT) {
 		//cout<<"check sum initialization started"<<endl;
@@ -330,15 +341,15 @@ magma_dpotrf_gpu(
 //        	int numOfCore = magma_get_lapack_numthreads();
 //        	cout<<"number of core=" << numOfCore<<endl;
 
-        	float real_time = 0.0;
-			float proc_time = 0.0;
-			long long flpins = 0.0;
-			float mflops = 0.0;
-			//timing start***************
-			if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
-				cout << "PAPI ERROR" << endl;
-				return -1;
-			}
+//        	float real_time = 0.0;
+//			float proc_time = 0.0;
+//			long long flpins = 0.0;
+//			float mflops = 0.0;
+//			//timing start***************
+//			if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+//				cout << "PAPI ERROR" << endl;
+//				return -1;
+//			}
             //=========================================================
             // Compute the Cholesky factorization A = L*L'.
             for (j=0; j < n; j += nb) {
