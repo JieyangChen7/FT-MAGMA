85de8b6bf6db42ad0e570ba4d2e6056221841cf3
Jieyang Chen <cjy7117@gmail.com>
2015-10-13 22:35:09 -0700

    test

--- magma-1.6.2-enhanced-kepler/magmablas/dgemmFT.cpp
+++ magma-1.6.2-enhanced-kepler/magmablas/dgemmFT.cpp
@@ -67,10 +67,10 @@ void dgemmFT(int m, int n, int k, double * A, int lda,
 		//magmablasSetKernelStream(streams[1]);
 		for (int i = 0; i < m; i += n) {
 			
-			magmablasSetKernelStream(streams[i%5]);
+			magmablasSetKernelStream(streams[2]);
 			magma_dgemv(MagmaTrans, n, k, MAGMA_D_ONE,
 					A + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
-			//magmablasSetKernelStream(streams[3]);
+			magmablasSetKernelStream(streams[3]);
 			magma_dgemv(MagmaTrans, n, k, MAGMA_D_ONE,
 					A + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );
 		}
