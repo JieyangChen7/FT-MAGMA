906ffb71cea6e2398cf8a20f9cacd05eb16b5a3a
Jieyang Chen <cjy7117@gmail.com>
2015-11-06 15:52:23 -0800

    test

--- magma-1.5.0/testing/testing_dpotrf_gpu.cpp
+++ magma-1.5.0/testing/testing_dpotrf_gpu.cpp
@@ -1,25 +1,60 @@
 /*
-    -- MAGMA (version 1.5.0) --
+    -- MAGMA (version 1.6.1) --
        Univ. of Tennessee, Knoxville
        Univ. of California, Berkeley
        Univ. of Colorado, Denver
-       @date September 2014
+       @date January 2015
 
-       @generated from testing_zpotrf_gpu.cpp normal z -> d, Wed Sep 17 15:08:40 2014
+       @generated from testing_zpotrf_gpu.cpp normal z -> d, Fri Jan 30 19:00:24 2015
 */
 // includes, system
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include <cuda_runtime_api.h>
-#include <cublas.h>
 
 // includes, project
 #include "flops.h"
 #include "magma.h"
 #include "magma_lapack.h"
 #include "testings.h"
+#include "cula.h"
+#include "papi.h"
+#include <iostream>
+
+using namespace std;
+
+int MeetsMinimumCulaRequirements()
+{
+    int cudaMinimumVersion = culaGetCudaMinimumVersion();
+    int cudaRuntimeVersion = culaGetCudaRuntimeVersion();
+    int cudaDriverVersion = culaGetCudaDriverVersion();
+    int cublasMinimumVersion = culaGetCublasMinimumVersion();
+    int cublasRuntimeVersion = culaGetCublasRuntimeVersion();
+
+    if(cudaRuntimeVersion < cudaMinimumVersion)
+    {
+        printf("CUDA runtime version is insufficient; "
+               "version %d or greater is required\n", cudaMinimumVersion);
+        return 0;
+    }
+
+    if(cudaDriverVersion < cudaMinimumVersion)
+    {
+        printf("CUDA driver version is insufficient; "
+               "version %d or greater is required\n", cudaMinimumVersion);
+        return 0;
+    }
+
+    if(cublasRuntimeVersion < cublasMinimumVersion)
+    {
+        printf("CUBLAS runtime version is insufficient; "
+               "version %d or greater is required\n", cublasMinimumVersion);
+        return 0;
+    }
+
+    return 1;
+}
 
 /* ////////////////////////////////////////////////////////////////////////////
    -- Testing dpotrf
@@ -30,33 +65,34 @@ int main( int argc, char** argv)
 
     real_Double_t   gflops, gpu_perf, gpu_time, cpu_perf, cpu_time;
     double *h_A, *h_R;
-    double *d_A;
+    double *resultMAGMA, *resultCULA;
+    magmaDouble_ptr d_A;
     magma_int_t N, n2, lda, ldda, info;
     double c_neg_one = MAGMA_D_NEG_ONE;
     magma_int_t ione     = 1;
-    magma_int_t ISEED[4] = {0,0,0,1};
+    magma_int_t ISEED[4] = {0,0,0,2};
     double      work[1], error;
     magma_int_t     status = 0;
 
     magma_opts opts;
-    parse_opts( argc, argv, &opts );
-    opts.lapack |= opts.check;  // check (-c) implies lapack (-l)
+	parse_opts( argc, argv, &opts );
+	opts.lapack |= opts.check;  // check (-c) implies lapack (-l)
     
     double tol = opts.tolerance * lapackf77_dlamch("E");
-    
-    printf("uplo = %s\n", lapack_uplo_const(opts.uplo) );
-    printf("  N     CPU GFlop/s (sec)   GPU GFlop/s (sec)   ||R_magma - R_lapack||_F / ||R_lapack||_F\n");
-    printf("========================================================\n");
-    for( int itest = 0; itest < opts.ntest; ++itest ) {
-        for( int iter = 0; iter < opts.niter; ++iter ) {
-            N   = opts.nsize[itest];
+    MeetsMinimumCulaRequirements();
+    int Nsize[] = {5120, 7680, 10240, 12800, 15360, 17920, 20480, 23040, 25600, 28160, 30720, 33280, 16};
+ 
+    for( int itest = 0; itest < 11; ++itest ) {
+            N   = Nsize[itest];
             lda = N;
             n2  = lda*N;
             ldda = ((N+31)/32)*32;
             gflops = FLOPS_DPOTRF( N ) / 1e9;
             
-            TESTING_MALLOC_CPU( h_A, double, n2     );
+            TESTING_MALLOC_PIN( h_A, double, n2     );
             TESTING_MALLOC_PIN( h_R, double, n2     );
+            TESTING_MALLOC_CPU( resultMAGMA, double, n2     );
+            TESTING_MALLOC_CPU( resultCULA, double, n2     );
             TESTING_MALLOC_DEV( d_A, double, ldda*N );
             
             /* Initialize the matrix */
@@ -65,54 +101,76 @@ int main( int argc, char** argv)
             lapackf77_dlacpy( MagmaUpperLowerStr, &N, &N, h_A, &lda, h_R, &lda );
             magma_dsetmatrix( N, N, h_A, lda, d_A, ldda );
             
-            /* ====================================================================
-               Performs operation using MAGMA
-               =================================================================== */
-            gpu_time = magma_wtime();
-            magma_dpotrf_gpu( opts.uplo, N, d_A, ldda, &info );
-            gpu_time = magma_wtime() - gpu_time;
-            gpu_perf = gflops / gpu_time;
-            if (info != 0)
-                printf("magma_dpotrf_gpu returned error %d: %s.\n",
-                       (int) info, magma_strerror( info ));
+            //testing MAGMA-----------------------------------------------------------
+            float real_time = 0.0;
+			float proc_time = 0.0;
+			long long flpins = 0.0;
+			float mflops = 0.0;
             
-            if ( opts.lapack ) {
-                /* =====================================================================
-                   Performs operation using LAPACK
-                   =================================================================== */
-                cpu_time = magma_wtime();
-                lapackf77_dpotrf( lapack_uplo_const(opts.uplo), &N, h_A, &lda, &info );
-                cpu_time = magma_wtime() - cpu_time;
-                cpu_perf = gflops / cpu_time;
-                if (info != 0)
-                    printf("lapackf77_dpotrf returned error %d: %s.\n",
-                           (int) info, magma_strerror( info ));
-                
-                /* =====================================================================
-                   Check the result compared to LAPACK
-                   =================================================================== */
-                magma_dgetmatrix( N, N, d_A, ldda, h_R, lda );
-                error = lapackf77_dlange("f", &N, &N, h_A, &lda, work);
-                blasf77_daxpy(&n2, &c_neg_one, h_A, &ione, h_R, &ione);
-                error = lapackf77_dlange("f", &N, &N, h_R, &lda, work) / error;
-                
-                printf("%5d   %7.2f (%7.2f)   %7.2f (%7.2f)   %8.2e   %s\n",
-                       (int) N, cpu_perf, cpu_time, gpu_perf, gpu_time,
-                       error, (error < tol ? "ok" : "failed") );
-                status += ! (error < tol);
-            }
-            else {
-                printf("%5d     ---   (  ---  )   %7.2f (%7.2f)     ---  \n",
-                       (int) N, gpu_perf, gpu_time );
-            }
-            TESTING_FREE_CPU( h_A );
+            if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+					cout << "PAPI ERROR" << endl;
+					return -1;
+            }            
+			
+			magma_dpotrf_gpu( MagmaLower, N, d_A, ldda, &info );
+			//magma_dpotrf( MagmaLower, N, h_A, ldda, &info );
+			
+            if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+					cout << "PAPI ERROR" << endl;
+					return -1;
+			}
+            cout << "MAGMA["<<N<<"]---time:"<<real_time<<"---gflops:"<<(double)gflops/real_time<<endl;
+      
+            PAPI_shutdown();
+            
+            magma_dgetmatrix( N, N, d_A, ldda, resultMAGMA, ldda );
+            
+            //testing CULA----------------------------------------------------------------
+            magma_dsetmatrix( N, N, h_A, lda, d_A, ldda );
+            culaInitialize();
+            
+            real_time = 0.0;
+			proc_time = 0.0;
+			flpins = 0.0;
+			mflops = 0.0;
+            
+			
+			if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+					cout << "PAPI ERROR" << endl;
+					return -1;
+			}
+			culaStatus status = culaDeviceDpotrf('l', N, d_A, ldda);
+			//culaStatus status = culaDpotrf('l', N, h_A, ldda);
+			if (status != culaNoError) {
+				cout<<"CULA ERROR:"<<status<<endl;
+			}
+			if (PAPI_flops(&real_time, &proc_time, &flpins, &mflops) < PAPI_OK) {
+				cout << "PAPI ERROR" << endl;
+				return -1;
+			}
+			cout << "CULA["<<N<<"]---time:"<<real_time<<"---gflops:"<<(double)gflops/real_time<<endl;
+			culaShutdown();
+			PAPI_shutdown();
+			
+			magma_dgetmatrix( N, N, d_A, ldda, resultCULA, ldda );
+			
+			double maxdiff = 0;
+			for (int i = 0; i < n2; i++) {
+				if (maxdiff < fabs(resultMAGMA[i] - resultCULA[i])) {
+					maxdiff = fabs(resultMAGMA[i] - resultCULA[i]);
+				}
+			}
+			cout << "max diff:" << maxdiff << endl;
+			
+			cout << endl;
+			
+            TESTING_FREE_PIN( h_A );
+            TESTING_FREE_CPU( resultMAGMA );
+            TESTING_FREE_CPU( resultCULA );
             TESTING_FREE_PIN( h_R );
             TESTING_FREE_DEV( d_A );
             fflush( stdout );
-        }
-        if ( opts.niter > 1 ) {
-            printf( "\n" );
-        }
+
     }
 
     TESTING_FINALIZE();
