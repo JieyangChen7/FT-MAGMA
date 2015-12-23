956b5dc581434d229dddf00c74f18f6875c55719
Jieyang Chen <cjy7117@gmail.com>
2015-12-22 18:49:23 -0800

    test

--- magma-1.6.2-enhanced-kepler/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-enhanced-kepler/magmablas/checksumGenerator.cpp
@@ -13,7 +13,7 @@ void initializeChecksum(double * matrix, int ld,
 	
 	for (int i = 0; i < N; i += B) {		
 		magma_dgemm(MagmaNoTrans, MagmaNoTrans,
-					2, N, B,
+					2, i+B, B,
 					MAGMA_D_ONE, vd, vd_ld,
 					matrix + i, ld,
 					MAGMA_D_ZERO, chksum + (i / B) * 2, chksum_ld);			
