240a95f4cd1d45a745a4ae835ea7b88c7923d1a8
Jieyang <cjy7117@gmail.com>
2015-12-27 15:05:30 -0800

    test

--- magma-1.6.2-enhanced-kepler/magmablas/dgemmFT.cpp
+++ magma-1.6.2-enhanced-kepler/magmablas/dgemmFT.cpp
@@ -57,13 +57,13 @@ void dgemmFT(int m, int n, int k, double * A, int lda,
 		
 	
 		//verify A before use
-		magmablasSetKernelStream(streams[1]);
+		//magmablasSetKernelStream(streams[1]);
 		for (int i = 0; i < m; i += n) {
 			
-			//magmablasSetKernelStream(streams[2]);
+			magmablasSetKernelStream(streams[2]);
 			magma_dgemv(MagmaTrans, n, k, MAGMA_D_ONE,
 					A + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
-			//magmablasSetKernelStream(streams[3]);
+			magmablasSetKernelStream(streams[3]);
 			magma_dgemv(MagmaTrans, n, k, MAGMA_D_ONE,
 					A + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );
 		}
@@ -96,8 +96,8 @@ void dgemmFT(int m, int n, int k, double * A, int lda,
 				C, ldc );
 	
 	if(FT){	
-		magmablasSetKernelStream(streams[1]);
-		//magmablasSetKernelStream(streams[4]);
+		//magmablasSetKernelStream(streams[1]);
+		magmablasSetKernelStream(streams[4]);
 		magma_dgemm(
 					MagmaNoTrans, MagmaTrans,
 					(m / n) * 2, n, k,
