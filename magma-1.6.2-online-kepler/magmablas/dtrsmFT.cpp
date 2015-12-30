612c5c46f33911c8b1ea0a8ffb66bd4d29e35b42
Jieyang Chen <cjy7117@gmail.com>
2015-12-30 00:51:45 -0800

    test

--- magma-1.6.2-online-kepler/magmablas/dtrsmFT.cpp
+++ magma-1.6.2-online-kepler/magmablas/dtrsmFT.cpp
@@ -34,14 +34,14 @@ void dtrsmFT(int m, int n, double * A, int lda,
 		
 		
 		//recalculate checksums
-		magma_queue_sync( streams[1] );
-		//magmablasSetKernelStream(streams[1]);
+		//magma_queue_sync( streams[1] );
+		magmablasSetKernelStream(streams[1]);
 		double beta = 0;
 		for (int i = 0; i < m; i += n) {
-			magmablasSetKernelStream(streams[2]);
+			//magmablasSetKernelStream(streams[2]);
 			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
 				B + i, ldb, vd, vd_ld, MAGMA_D_ZERO, chk1 + (i / n), chk1_ld );
-			magmablasSetKernelStream(streams[3]);
+			//magmablasSetKernelStream(streams[3]);
 			magma_dgemv(MagmaTrans, n, n, MAGMA_D_ONE,
 				B + i, ldb, vd + 1, vd_ld, MAGMA_D_ZERO, chk2 + (i / n), chk2_ld );	
 		}
