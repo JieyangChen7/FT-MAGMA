51601a4d6b68955ea9b017292d5741c3141e4e8b
Jieyang Chen <cjy7117@gmail.com>
2015-12-30 00:21:28 -0800

    test

--- magma-1.6.2-online-kepler/magmablas/dgemmFT.cpp
+++ magma-1.6.2-online-kepler/magmablas/dgemmFT.cpp
@@ -82,15 +82,15 @@ void dgemmFT(int m, int n, int k, double * A, int lda,
 			printMatrix_gpu(checksumC, checksumC_ld, (m / n) * 2, n);
 		}
 		
-//		magma_queue_sync( streams[2] );
-//		magma_queue_sync( streams[3] );
-//		magma_queue_sync( streams[4] );
-//		//error detection and error correction
-//		ErrorDetectAndCorrect(C, ldc, n, m, n,
-//				checksumC, checksumC_ld,
-//				chk1, chk1_ld,
-//				chk2, chk2_ld,
-//				streams[1]);
+		magma_queue_sync( streams[2] );
+		magma_queue_sync( streams[3] );
+		magma_queue_sync( streams[4] );
+		//error detection and error correction
+		ErrorDetectAndCorrect(C, ldc, n, m, n,
+				checksumC, checksumC_ld,
+				chk1, chk1_ld,
+				chk2, chk2_ld,
+				streams[1]);
 				
 		
 	}
