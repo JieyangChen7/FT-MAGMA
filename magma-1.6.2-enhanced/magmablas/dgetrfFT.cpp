37d080ed79ab5acf6cf7ff77c76b1718640c78f6
Jieyang Chen <cjy7117@gmail.com>
2016-06-03 12:51:26 -0700

    added checksum update for dgetrf

--- magma-1.6.2-enhanced/magmablas/dgetrfFT.cpp
+++ magma-1.6.2-enhanced/magmablas/dgetrfFT.cpp
@@ -4,7 +4,16 @@
 using namespace std;
 
 //LU Factorization with FT on CPU using ACML
+//m: number of row
+//n: number of col
 
+void row_swap(double * chksum, int chksum_ld, int n, int i, int j) {
+	for (int k = 0; k < n; k++) {
+		double temp = *(chksum + k * chksum_ld + i);
+		*(chksum + k * chksum_ld + i) = *(chksum + k * chksum_ld + j);
+		*(chksum + k * chksum_ld + j) = temp;
+	}
+}
 
 void dgetrfFT(int m, int n, double * A, int lda, int * ipiv, int * infom
               double * chksum, int chksum_ld,
@@ -15,7 +24,31 @@ void dgetrfFT(int m, int n, double * A, int lda, int * ipiv, int * infom
     lapackf77_dgetrf( &m, &n, A, &lda, ipiv, info);
     
     if (FT) {
-        
+        //update checksums
+        for (int j = 0; j < n; j++) {
+        	//swap row j with ipiv[j]
+        	row_swap(chksum, chksum_ld, 2, j, ipiv[j]);
+        	double Ajj = *(A + j * lda + j);
+        	if (Ajj != 0) {
+        		double r = *(chksum + j) * (-1);
+        		int inc = 1;
+        		blasf77_daxpy(m - j - 1, &r, 
+        			A + j * lda + j + 1, &inc,
+        			chksum + j + 1, &inc);
+
+				r = *(chksum + chksum_ld + j) * (-1);
+        		blasf77_daxpy(m - j - 1, &r, 
+        			A + j * lda + j + 1, &inc,
+        			chksum + chksum_ld + j + 1, &inc);
+        	}
+        }
+
+        if (DEBUG) {
+        	cout << "[dgetrf] updated matrix:" << endl;
+        	printMatrix_host(A, lda,  m, n);
+        	cout << "[dgetrf] updated checksum:" << endl;
+        	printMatrix_host(chksum, chksum_ld,  m, 2);
+        }
     }
 }
 
