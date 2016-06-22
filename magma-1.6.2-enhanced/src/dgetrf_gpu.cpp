95fb5829a9fdc16016ea54c79cdd7914f2e20745
Jieyang Chen <cjy7117@gmail.com>
2016-06-22 16:32:36 -0700

    test

--- magma-1.6.2-enhanced/src/dgetrf_gpu.cpp
+++ magma-1.6.2-enhanced/src/dgetrf_gpu.cpp
@@ -397,7 +397,10 @@ magma_dgetrf_gpu(
                 // also do row swap on checksums
                 //magmablas_dlaswp( (n/nb)*2, checksum, checksum_ld, j*nb + 1, j*nb + nb, ipiv, 1 );
                 cout<<"[ipiv] ipiv:"<<endl;
-                printMatrix_gpu(ipiv, 1, 1, n);
+                for (int i = 0; i < m; i++) {
+                    cout << ipiv[i] << " ";
+                }
+                cout << endl;
 
             }
 
