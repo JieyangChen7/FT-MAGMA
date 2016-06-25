61e9cc1c08403269ff5b23036e02fa9cde9ab779
Jieyang Chen <cjy7117@gmail.com>
2016-06-25 11:46:49 -0700

    test

--- magma-1.6.2-enhanced/test.sh
+++ magma-1.6.2-enhanced/test.sh
@@ -1,13 +1,13 @@
 #./testing/testing_dgetrf_gpu -N 16,16
-#./testing/testing_dgetrf_gpu -N 10240,10240 
+./testing/testing_dgetrf_gpu -N 10240,10240 
 
 
-rm profile
-touch profile
-chmod 777 profile
+# rm profile
+# touch profile
+# chmod 777 profile
 
-nvprof --profile-from-start off -o profile ./testing/testing_dgetrf_gpu -N 20480,20480
+# nvprof --profile-from-start off -o profile ./testing/testing_dgetrf_gpu -N 20480,20480
 
-git add profile
-git commit -m "updated profile"
-git push origin master
+# git add profile
+# git commit -m "updated profile"
+# git push origin master
