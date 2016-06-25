c1f95ea11ca7a0fadb30af5b9eba591954a3ae0d
Jieyang Chen <cjy7117@gmail.com>
2016-06-25 12:33:20 -0700

    test

--- magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
@@ -414,29 +414,28 @@ cudaProfilerStart();
 			// gpu_time6 = magma_wtime() - gpu_time6;
 
 
-			gpu_time7 = magma_wtime();
+			// gpu_time7 = magma_wtime();
+			// for (int k = 0; k < K; k ++){
+			// recalculateChecksum(A, lda,
+			// 			i, j, chk_nb,
+			// 			vd2, vd2_ld,
+			//    			chk21, chk21_ld, 
+			//    			chk22, chk22_ld, 
+			//    			streams);
+			// }
+			// gpu_time7 = magma_wtime() - gpu_time7;
+			
+
+
+			gpu_time8 = magma_wtime();
 			for (int k = 0; k < K; k ++){
-			recalculateChecksum(A, lda,
+			recalculateChecksum2(A, lda,
 						i, j, chk_nb,
 						vd2, vd2_ld,
 			   			chk21, chk21_ld, 
 			   			chk22, chk22_ld, 
 			   			streams);
 			}
-			gpu_time7 = magma_wtime() - gpu_time7;
-			//cout << gpu_time <<"	";
-
-
-			// gpu_time8 = magma_wtime();
-			// for (int k = 0; k < K; k ++){
-			// recalculateChecksum2(A, lda,
-			// 			i, j, chk_nb,
-			// 			vd2, vd2_ld,
-			//    			chk21, chk21_ld, 
-			//    			chk22, chk22_ld, 
-			//    			streams);
-
-			// }
 			gpu_time8 = magma_wtime() - gpu_time8;
 
 			double min_time1 = fmin(gpu_time1, fmin(gpu_time2, fmin(gpu_time3, gpu_time4)));
