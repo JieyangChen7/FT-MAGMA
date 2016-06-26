eacf2f286b02c5bf0b8ea6018e5be025c04bf5a5
Jieyang Chen <cjy7117@gmail.com>
2016-06-25 19:30:52 -0700

    test

--- magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
@@ -402,16 +402,16 @@ cudaProfilerStart();
 
 
 
-			// gpu_time6 = magma_wtime();
-			// for (int k = 0; k < K; k ++){
-			// recalculateChecksum6(A, lda,
-			// 			i, j, chk_nb,
-			// 			vd, vd_ld,
-			//    			chk21, chk21_ld, 
-			//    			chk22, chk22_ld, 
-			//    			streams);
-			// }
-			// gpu_time6 = magma_wtime() - gpu_time6;
+			gpu_time6 = magma_wtime();
+			for (int k = 0; k < K; k ++){
+			recalculateChecksum6(A, lda,
+						i, j, chk_nb,
+						vd, vd_ld,
+			   			chk21, chk21_ld, 
+			   			chk22, chk22_ld, 
+			   			streams);
+			}
+			gpu_time6 = magma_wtime() - gpu_time6;
 
 
 			gpu_time7 = magma_wtime();
