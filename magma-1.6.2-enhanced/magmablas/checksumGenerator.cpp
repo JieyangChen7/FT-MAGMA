97b91ca85d0ecc178db46cff546dd738b026d4cb
Jieyang Chen <cjy7117@gmail.com>
2016-06-25 22:48:18 -0700

    test

--- magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-enhanced/magmablas/checksumGenerator.cpp
@@ -304,7 +304,7 @@ void recalculateChecksum8(double * A, int lda,
 	for (int i = 0; i < m; i += chk_nb) {
 		magmablasSetKernelStream(streams[1]);
 		magma_dgemm(MagmaTrans, MagmaNoTrans,
-					n, 2, chk_nb,
+					10240, 2, chk_nb,
 					//2, B + i, B,
 					MAGMA_D_ONE, 
 					A + i, lda,
@@ -426,16 +426,16 @@ cudaProfilerStart();
 			
 
 
-			// gpu_time8 = magma_wtime();
-			// for (int k = 0; k < K; k ++){
-			// recalculateChecksum8(A, lda,
-			// 			i, j, chk_nb,
-			// 			vd2, vd2_ld,
-			//    			chk21, chk21_ld, 
-			//    			chk22, chk22_ld, 
-			//    			streams);
-			// }
-			// gpu_time8 = magma_wtime() - gpu_time8;
+			gpu_time8 = magma_wtime();
+			for (int k = 0; k < K; k ++){
+			recalculateChecksum8(A, lda,
+						i, j, chk_nb,
+						vd2, vd2_ld,
+			   			chk21, chk21_ld, 
+			   			chk22, chk22_ld, 
+			   			streams);
+			}
+			gpu_time8 = magma_wtime() - gpu_time8;
 
 			double min_time1 = fmin(gpu_time1, fmin(gpu_time2, fmin(gpu_time3, gpu_time4)));
 			double min_time2 = fmin(gpu_time5, fmin(gpu_time6, fmin(gpu_time7, gpu_time8)));
