450e24f4a45b0c08cd5a1aa7185c73032ac50682
Jieyang Chen <cjy7117@gmail.com>
2016-07-16 18:16:06 -0700

    test

--- magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
+++ magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
@@ -398,10 +398,30 @@ magma_dgetrf_gpu(
                     }
                     cout << endl; 
                 }
-                row_chk_swap(abftEnv, dAT, lddat, real_effect);
+               // row_chk_swap(abftEnv, dAT, lddat, real_effect);
 
             }
 
+////////
+            cout << "before" << endl;
+            int mem_row = nb; // number of row and col of B stored in memory(no trans operation)
+            int mem_col = nb;
+
+        at_col_chk_recal(abftEnv, dAT(j, j+1), lddat, nb, nb);
+
+        col_detect_correct(dAT(j, j+1), lddat, abftEnv->chk_nb, nb, nb,
+                              COL_CHK_T(j, j+1), abftEnv->col_dchk_ld,
+                              abftEnv->chk1, abftEnv->chk1_ld,
+                              abftEnv->chk2, abftEnv->chk2_ld,
+                              abftEnv->stream[1]);
+
+        col_debug(dAT(j, j+1), lddat, abftEnv->chk_nb, nb, nb,
+                              COL_CHK_T(j, j+1), abftEnv->col_dchk_ld,
+                              abftEnv->chk1, abftEnv->chk1_ld,
+                              abftEnv->chk2, abftEnv->chk2_ld,
+                              abftEnv->stream[1]);
+
+////////
             magmablas_dlaswp( n, dAT, lddat, j*nb + 1, j*nb + nb, ipiv, 1 );
 
             if (FT) {
@@ -411,6 +431,30 @@ magma_dgetrf_gpu(
                                   j*nb + 1, j*nb + nb, 
                                   ipiv, 1 );
             }
+
+
+////////
+            cout << "after" << endl;
+            int mem_row = nb; // number of row and col of B stored in memory(no trans operation)
+            int mem_col = nb;
+            magma_queue_sync( stream[1] );
+            at_col_chk_recal(abftEnv, dAT(j, j+1), lddat, nb, nb);
+
+            col_detect_correct(dAT(j, j+1), lddat, abftEnv->chk_nb, nb, nb,
+                                  COL_CHK_T(j, j+1), abftEnv->col_dchk_ld,
+                                  abftEnv->chk1, abftEnv->chk1_ld,
+                                  abftEnv->chk2, abftEnv->chk2_ld,
+                                  abftEnv->stream[1]);
+
+            col_debug(dAT(j, j+1), lddat, abftEnv->chk_nb, nb, nb,
+                                  COL_CHK_T(j, j+1), abftEnv->col_dchk_ld,
+                              abftEnv->chk1, abftEnv->chk1_ld,
+                              abftEnv->chk2, abftEnv->chk2_ld,
+                              abftEnv->stream[1]);
+
+////////
+
+
 
             magma_queue_sync( stream[0] );
 
