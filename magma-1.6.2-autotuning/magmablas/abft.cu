f11eff57f16a8d3e951038da645712c3b333ddbd
Jieyang Chen <cjy7117@gmail.com>
2016-07-16 21:57:13 -0700

    test

--- magma-1.6.2-autotuning/magmablas/abft.cu
+++ magma-1.6.2-autotuning/magmablas/abft.cu
@@ -167,11 +167,11 @@ void col_debug(double * A, int lda, int B, int m, int n,
 				double r1 = *(chk1_host + j * (m/B) + i);
 				double r2 = *(chk2_host + j * (m/B) + i);
 				//if (i < 16 && j < 16) {
-				if (abs(u1-r1) > 1e-10) {
+				if (fabs(u1-r1) > 1e-10) {
 
 					printf("%d,%d,%f-%ferror1\n", i, j, u1, r1 );
 				}
-				if (abs(u2-r2) > 1e-10) {
+				if (fabs(u2-r2) > 1e-10) {
 					printf("%d,%d,%f-%ferror2\n", i, j, u2, r2);
 				}
 				//}
