205fa3bd94b8077759a03d0135f4e1faad4a5697
Jieyang Chen <cjy7117@gmail.com>
2016-07-17 12:35:28 -0700

    test

--- magma-1.6.2-autotuning/src/dpotrf_gpu.cpp
+++ magma-1.6.2-autotuning/src/dpotrf_gpu.cpp
@@ -119,7 +119,7 @@ magma_dpotrf_gpu(
     nb = magma_get_dpotrf_nb(n);
 
     //** debug **//
-    // nb = 4;
+     nb = 4;
         
         
     if (MAGMA_SUCCESS != magma_dmalloc_pinned( &work, nb*nb )) {
@@ -149,7 +149,7 @@ magma_dpotrf_gpu(
 
     //variables for FT
     bool FT = true;
-    bool DEBUG = false;
+    bool DEBUG = true;
 
     ABFTEnv * abftEnv;
     abftEnv = new ABFTEnv();
