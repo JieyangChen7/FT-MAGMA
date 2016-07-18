bc95226e713c1db4e72b6bacc93325e0a372c495
Jieyang Chen <cjy7117@gmail.com>
2016-07-18 01:02:23 -0700

    test

--- magma-1.6.2-autotuning/test.sh
+++ magma-1.6.2-autotuning/test.sh
@@ -3,7 +3,7 @@
 #bdz K40c - 33000
 #bdz C2050 - 18000
 #tardis M2075 - 28000
-./testing/testing_dgetrf_gpu -N 10240,10340
+./testing/testing_dgetrf_gpu -N 20480,20480
 
 #./testing/testing_dpotrf_gpu -N 30720,30720
 
