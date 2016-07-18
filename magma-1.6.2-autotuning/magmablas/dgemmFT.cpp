8a862d8bd75dc05bcea5d20908222bdd474d7369
Jieyang Chen <cjy7117@gmail.com>
2016-07-17 23:22:33 -0700

    test

--- magma-1.6.2-autotuning/magmablas/dgemmFT.cpp
+++ magma-1.6.2-autotuning/magmablas/dgemmFT.cpp
@@ -168,13 +168,13 @@ void dgemmFT( magma_trans_t transA, magma_trans_t transB,
 					col_chkC, col_chkC_ld );
 
 		//we can further work on this to support trans A.
-		// magma_dgemm(transA, transB,
-		// 			m , (n / abftEnv->chk_nb) * 2, k,
-		// 			alpha,
-		// 			A, lda,
-		// 			row_chkB, row_chkB_ld,
-		// 			beta,
-		// 			row_chkC, row_chkC_ld );
+		magma_dgemm(transA, transB,
+					m , (n / abftEnv->chk_nb) * 2, k,
+					alpha,
+					A, lda,
+					col_chkB, col_chkB_ld,
+					beta,
+					row_chkC, row_chkC_ld );
 	}
 
 
