872c2e90319a68b2bef36f1093d7d9a69b576581
Jieyang Chen <cjy7117@gmail.com>
2016-08-24 08:33:26 +0800

    test

--- magma-1.6.2-autotuning/magmablas/checksumGenerator.cpp
+++ magma-1.6.2-autotuning/magmablas/checksumGenerator.cpp
@@ -264,8 +264,9 @@ void initializeABFTEnv(ABFTEnv * abftEnv, int chk_nb,
 
 
 	cout << "updatedCounter initialize" << endl;
-    abftEnv->updatedCounter = new int[(abftEnv->gpu_row/abftEnv->chk_nb) * (abftEnv->gpu_col/abftEnv->chk_nb)]();
+    abftEnv->updatedCounter = new int[(abftEnv->gpu_row/abftEnv->chk_nb) * (abftEnv->gpu_col/abftEnv->chk_nb)];
     abftEnv->updatedCounter_ld = abftEnv->gpu_row/abftEnv->chk_nb;
+    memset(abftEnv->updatedCounter, 0,(abftEnv->gpu_row/abftEnv->chk_nb) * (abftEnv->gpu_col/abftEnv->chk_nb) * sizeof(double) );
     cout << "done." << endl;
 
     //to he auto tuned later
