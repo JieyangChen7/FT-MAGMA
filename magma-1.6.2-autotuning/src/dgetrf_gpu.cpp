6f5a776c36290b8b30ab4725280b1faf190d9633
Jieyang Chen <cjy7117@gmail.com>
2016-08-25 19:29:42 +0800

    test

--- magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
+++ magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
@@ -211,7 +211,7 @@ magma_dgetrf_gpu(
         }
   
         /* flags */
-        bool FT = true;
+        bool FT = false;
         bool DEBUG = false;
         bool VERIFY = true;
     
@@ -299,7 +299,7 @@ magma_dgetrf_gpu(
                 //                     dAT(j-1,j+1), lddat );
 
                 VERIFY = updateCounter(abftEnv, j + 1, n / nb - 1, j - 1, j - 1, 1);
-                
+
                 dtrsmFT( MagmaRight, MagmaUpper, MagmaNoTrans, MagmaUnit,
                              n - (j+1)*nb, nb,
                              c_one, dAT(j-1,j-1), lddat,
