11246ee31ff536ecce80d7ade73017de2a76ad82
Jieyang Chen <cjy7117@gmail.com>
2016-08-25 19:36:27 +0800

    test

--- magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
+++ magma-1.6.2-autotuning/src/dgetrf_gpu.cpp
@@ -331,7 +331,7 @@ magma_dgetrf_gpu(
                              ROW_CHK_T(j, j-1), abftEnv->row_dchk_ld,
                              COL_CHK_T(j, j+1), abftEnv->col_dchk_ld,
                              ROW_CHK_T(j, j+1), abftEnv->row_dchk_ld,
-                             FT, DEBUG, VERIFY, stream);
+                             true, DEBUG, VERIFY, stream);
             }
 
             // do the cpu part
@@ -464,7 +464,7 @@ magma_dgetrf_gpu(
                         ROW_CHK_T(j+1, j), abftEnv->row_dchk_ld,
                         COL_CHK_T(j+1, j+1), abftEnv->col_dchk_ld,
                         ROW_CHK_T(j+1, j+1), abftEnv->row_dchk_ld,
-                        FT, DEBUG, VERIFY, stream);
+                        true, DEBUG, VERIFY, stream);
 
             }
             else if (n-s*nb > 0){
