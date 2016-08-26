f6a9a019f989422a9312c35ef7346a4b297e5984
Jieyang Chen <cjy7117@gmail.com>
2016-08-26 22:14:23 +0800

    test

--- magma-1.6.2-autotuning/test.sh
+++ magma-1.6.2-autotuning/test.sh
@@ -18,7 +18,7 @@
 #./testing/testing_dpotrf_gpu -N 15360,15360
 #./testing/testing_dpotrf_gpu -N 17920,17920
 #./testing/testing_dpotrf_gpu -N 20480,20480
-#./testing/testing_dpotrf_gpu -N 23040,23040
+./testing/testing_dpotrf_gpu -N 23040,23040
 
 
 
@@ -32,14 +32,14 @@
 #./testing/testing_dgetrf_gpu -N 23040,23040
 
 
-./testing/testing_dgeqrf_gpu -N 5120,5120
-./testing/testing_dgeqrf_gpu -N 7680,7680
-./testing/testing_dgeqrf_gpu -N 10240,10240
-./testing/testing_dgeqrf_gpu -N 12800,12800
-./testing/testing_dgeqrf_gpu -N 15360,15360
-./testing/testing_dgeqrf_gpu -N 17920,17920
-./testing/testing_dgeqrf_gpu -N 20480,20480
-./testing/testing_dgeqrf_gpu -N 23040,23040
+#./testing/testing_dgeqrf_gpu -N 5120,5120
+#./testing/testing_dgeqrf_gpu -N 7680,7680
+#./testing/testing_dgeqrf_gpu -N 10240,10240
+#./testing/testing_dgeqrf_gpu -N 12800,12800
+#./testing/testing_dgeqrf_gpu -N 15360,15360
+#./testing/testing_dgeqrf_gpu -N 17920,17920
+#./testing/testing_dgeqrf_gpu -N 20480,20480
+#./testing/testing_dgeqrf_gpu -N 23040,23040
 
 
 
