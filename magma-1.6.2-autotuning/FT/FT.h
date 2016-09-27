aec35a15b8544631850694dedac7d3e044e649db
Jieyang Chen <cjy7117@gmail.com>
2016-09-27 16:16:06 -0700

    test

--- magma-1.6.2-autotuning/FT/FT.h
+++ magma-1.6.2-autotuning/FT/FT.h
@@ -109,7 +109,7 @@ void CholeskyGenerator(double * A, int lda, int n);
 
 void printMatrix_host(double * matrix_host, int ld,  int M, int N, int row_block, int col_block);
 void printMatrix_host_int(int * matrix_host, int ld,  int M, int N, int row_block, int col_block);
-void printMatrix_host_time(time_t * matrix_host, int ld,  int M, int N, int row_block, int col_block)
+void printMatrix_host_time(time_t * matrix_host, int ld,  int M, int N, int row_block, int col_block);
 void printMatrix_gpu(double * matrix_device, int matrix_ld, int M, int N, int row_block, int col_block);
 
 void printVector_host(double * vector_host, int N);
