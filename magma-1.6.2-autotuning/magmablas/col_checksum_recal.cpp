3444e34d101b7eccaf785e4971599e73107d1806
Jieyang Chen <cjy7117@gmail.com>
2016-12-28 12:49:55 -0800

    test

--- magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
+++ magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
@@ -713,21 +713,28 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 
 			benchmark_time = magma_wtime();
 			for (int t = 0; t < 100; t++) {
-				col_chk_recal_select(abftEnv, A, lda, i, i, 9);
+				col_chk_recal_select(abftEnv, A, lda, abftEnv->chk_nb, i, 9);
 			}
 			benchmark_time = magma_wtime() - benchmark_time;
 			cout << benchmark_time << "\t";
 
 			benchmark_time = magma_wtime();
 			for (int t = 0; t < 100; t++) {
-				col_chk_recal_select(abftEnv, A, lda, i, i, 1);
+				col_chk_recal_select(abftEnv, A, lda, abftEnv->chk_nb, i, 1);
 			}
 			benchmark_time = magma_wtime() - benchmark_time;
 			cout << benchmark_time << "\t";
 
 			benchmark_time = magma_wtime();
 			for (int t = 0; t < 100; t++) {
-				col_chk_recal_select(abftEnv, A, lda, i, i, 7);
+				col_chk_recal_select(abftEnv, A, lda, abftEnv->chk_nb, i, 7);
+			}
+			benchmark_time = magma_wtime() - benchmark_time;
+			cout << benchmark_time << endl;
+
+			benchmark_time = magma_wtime();
+			for (int t = 0; t < 100; t++) {
+				col_chk_recal_select(abftEnv, A, lda, abftEnv->chk_nb, i, 13);
 			}
 			benchmark_time = magma_wtime() - benchmark_time;
 			cout << benchmark_time << endl;
