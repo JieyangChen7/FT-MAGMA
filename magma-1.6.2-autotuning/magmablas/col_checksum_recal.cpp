92173e1ad9453b18a1b38faf42834bc9979f004f
Jieyang Chen <cjy7117@gmail.com>
2016-12-28 14:23:13 -0800

    test

--- magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
+++ magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
@@ -776,18 +776,18 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 			//benchmark_time = magma_wtime();
 			//for (int t = 0; t < 1; t++) {
 
-			col_checksum_kernel_ncns2(abftEnv->chk_nb, abftEnv->chk_nb, abftEnv->chk_nb,
-						  A, lda, 
-						  abftEnv->hrz_vd, abftEnv->hrz_vd_ld, 
-						  test_chk2, test_chk2_ld, 
-						  abftEnv->stream);
-			cudaStreamSynchronize(*(abftEnv->stream));
+			// col_checksum_kernel_ncns2(abftEnv->chk_nb, abftEnv->chk_nb, abftEnv->chk_nb,
+			// 			  A, lda, 
+			// 			  abftEnv->hrz_vd, abftEnv->hrz_vd_ld, 
+			// 			  test_chk2, test_chk2_ld, 
+			// 			  abftEnv->stream);
+			// cudaStreamSynchronize(*(abftEnv->stream));
 
-			chkenc(A, lda, abftEnv->chk_nb, abftEnv->chk_nb, test_chk1, test_chk1_ld, 
-						  *(abftEnv->stream));
-			cudaStreamSynchronize(*(abftEnv->stream));
+			// chkenc(A, lda, abftEnv->chk_nb, abftEnv->chk_nb, test_chk1, test_chk1_ld, 
+			// 			  *(abftEnv->stream));
+			// cudaStreamSynchronize(*(abftEnv->stream));
 
-			compareChk(test_chk1, test_chk1_ld, test_chk2, test_chk2_ld, 2, abftEnv->chk_nb);
+			// compareChk(test_chk1, test_chk1_ld, test_chk2, test_chk2_ld, 2, abftEnv->chk_nb);
 
 			printMatrix_gpu(test_chk2, test_chk2_ld,  2 , abftEnv->chk_nb, 2, 4);
 
