d621dbe1834d999371b4fd2e08ca5aad5e620323
Jieyang Chen <cjy7117@gmail.com>
2016-12-28 15:18:22 -0800

    test

--- magma-1.6.2-autotuning/test.sh
+++ magma-1.6.2-autotuning/test.sh
@@ -29,9 +29,9 @@
 
 
 
-#./testing/testing_dgetrf_gpu -N 5120,5120
+./testing/testing_dgetrf_gpu -N 5120,5120
 #./testing/testing_dgetrf_gpu -N 7680,7680
-./testing/testing_dgetrf_gpu -N 10240,10240
+#./testing/testing_dgetrf_gpu -N 10240,10240
 #./testing/testing_dgetrf_gpu -N 12800,12800
 #./testing/testing_dgetrf_gpu -N 15360,15360
 #./testing/testing_dgetrf_gpu -N 17920,17920
