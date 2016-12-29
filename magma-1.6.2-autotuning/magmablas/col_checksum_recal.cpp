39412514a268f5aaca6ba49b73320ac1751142e5
Jieyang Chen <cjy7117@gmail.com>
2016-12-28 16:27:01 -0800

    test

--- magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
+++ magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
@@ -801,9 +801,9 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 			compareChk(test_chk1, test_chk1_ld, abftEnv->hrz_recal_chk, abftEnv->hrz_recal_chk_ld, 2, i);
 
 			if (i == 1024)
-			 printMatrix_gpu(abftEnv->hrz_recal_chk, abftEnv->hrz_recal_chk_ld,  2 , i, 2, 4);
+			 printMatrix_gpu(abftEnv->hrz_recal_chk, abftEnv->hrz_recal_chk_ld,  2 , 512, 2, 4);
 
-			 printMatrix_gpu(test_chk1, test_chk1_ld,  2 , i, 2, 4);
+			 printMatrix_gpu(test_chk1, test_chk1_ld,  2 , 512, 2, 4);
 
 			cout << endl;
 
