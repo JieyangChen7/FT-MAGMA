3dc9bcbfe438570f71c7958aafff511a1a5f51eb
Jieyang Chen <cjy7117@gmail.com>
2016-12-28 16:15:06 -0800

    test

--- magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
+++ magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
@@ -777,7 +777,7 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 			benchmark_time = magma_wtime();
 			for (int t = 0; t < 1; t++) {
 
-			col_checksum_kernel_ncns2(abftEnv->chk_nb, abftEnv->chk_nb, abftEnv->chk_nb,
+			col_checksum_kernel_ncns2(abftEnv->chk_nb, i, abftEnv->chk_nb,
 						  A, lda, 
 						  abftEnv->hrz_vd, abftEnv->hrz_vd_ld, 
 						  test_chk2, test_chk2_ld, 
@@ -788,7 +788,7 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 						  *(abftEnv->stream));
 			cudaStreamSynchronize(*(abftEnv->stream));
 
-			compareChk(test_chk1, test_chk1_ld, test_chk2, test_chk2_ld, 2, abftEnv->chk_nb);
+			compareChk(test_chk1, test_chk1_ld, test_chk2, test_chk2_ld, 2, i);
 
 			// printMatrix_gpu(test_chk2, test_chk2_ld,  2 , abftEnv->chk_nb, 2, 4);
 
