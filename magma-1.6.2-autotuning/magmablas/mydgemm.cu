b414faae56eca6e998a3cf9ef6c247b00766a3e2
Jieyang Chen <cjy7117@gmail.com>
2016-12-30 22:04:29 -0800

    test

--- magma-1.6.2-autotuning/magmablas/mydgemm.cu
+++ magma-1.6.2-autotuning/magmablas/mydgemm.cu
@@ -253,7 +253,7 @@ void chkenc(double * A, int lda, int m, int n, double * chk , int ldchk, magma_q
 	//int cb = 16;
 	//dim3 d(rb, cb, 1);
 	//chkenc_kernel3_5<<<N/cb, d, rb*cb*sizeof(double), stream>>>(A, lda, chk, ldchk);
-	chkenc_kernel3<<<n/B, B, 0, stream>>>(A, lda, Chk, ldchk);
+	chkenc_kernel3<<<n/B, B, 0, stream>>>(A, lda, chk, ldchk);
 
 }
 
