a448334ef193ef11d328b9980d4eef3f0e5afeaf
Jieyang Chen <cjy7117@gmail.com>
2017-01-01 21:42:12 -0800

    test

--- magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
+++ magma-1.6.2-autotuning/magmablas/col_checksum_recal.cpp
@@ -750,7 +750,7 @@ void col_benchmark_single(ABFTEnv * abftEnv, double * A, int lda){
 	for (int i = abftEnv->chk_nb; i <= abftEnv->gpu_col; i += abftEnv->chk_nb) {
     //int i = abftEnv->chk_nb;
 	//	for (int j = abftEnv->chk_nb; j < abftEnv->gpu_col; j += abftEnv->chk_nb) {
-		long long flops = 3 * abftEnv->chk_nb * i;
+		long long flops = 3 * i * i;
 		flops *= 1;
 		double t1 =0;
 		double t2 =0;
