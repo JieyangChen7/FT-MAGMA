4b0efdd3a9c81ae4c70c00cad66d02b16d396b3e
Jieyang Chen <cjy7117@gmail.com>
2017-01-08 16:46:28 -0700

    test

--- magma-1.6.2-autotuning/magmablas/dgeqrfFT.cpp
+++ magma-1.6.2-autotuning/magmablas/dgeqrfFT.cpp
@@ -45,55 +45,118 @@ void dgeqrfFT( int m, int n, double * A, int lda, double * tau, double * work, i
 	int k = min(m, n);
 	for (int i = 0; i < k; i++) {
 		cout << "i=" << i <<endl;
+
+
+
 		int m2 = m - i;
-		int n2 = n - i - 1;
+		//int n2 = n - i - 1;
+		int n2 = n - i;
 		int incx = 1;
+		double Aii = *(A + i * lda + i);
+		double * v = new double[m2];
+		memcpy(v+1, A + i * lda + i + 1, (m2-1)* sizeof(double));
+
+		// lapackf77_dlarfg(&m2,
+  //                        A + i * lda + i,
+  //                        A + i * lda + i + 1, &incx,
+  //                        tau + i);
+
 		lapackf77_dlarfg(&m2,
-                         A + i * lda + i,
-                         A + i * lda + i + 1, &incx,
-                         tau + i);
-		double t = *(tau + i);
-		double b = *(A + i * lda + i);
-		double scale = 1/(-(t * b));
-		cout << "scale:" << 1/(-(t * b)) <<endl;
+                 &Aii,
+                 v+1, &incx,
+                 tau + i);
+		v[0] = 1;
 
-		int n3 = n-i;
-		double alpha = -1;
-		blasf77_daxpy(&n3,
-                      &alpha,
-                      A + i * lda + i, &lda,
-                      abftEnv->col_hchk + i * abftEnv->col_hchk_ld, &abftEnv->col_hchk_ld);
+		// double t = *(tau + i);
+		// double b = *(A + i * lda + i);
+		// double scale = 1/(-(t * b));
+		// cout << "scale:" << 1/(-(t * b)) <<endl;
 
-		alpha = -1 * (i+1);
-		blasf77_daxpy(&n3,
-                      &alpha,
-                      A + i * lda + i, &lda,
-                      abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1, &abftEnv->col_hchk_ld);
-	
-		int n4 = 2;
-		int incxx = 1;
-		blasf77_dscal(&n4,
-                      &scale,
-                      abftEnv->col_hchk + i * abftEnv->col_hchk_ld, &incxx );
+		// int n3 = n-i;
+		// double alpha = -1;
+		// blasf77_daxpy(&n3,
+  //                     &alpha,
+  //                     A + i * lda + i, &lda,
+  //                     abftEnv->col_hchk + i * abftEnv->col_hchk_ld, &abftEnv->col_hchk_ld);
+
+		// alpha = -1 * (i+1);
+		// blasf77_daxpy(&n3,
+  //                     &alpha,
+  //                     A + i * lda + i, &lda,
+  //                     abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1, &abftEnv->col_hchk_ld);
+
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld) -= 1 * Aii;
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1) -= (i + 1) * Aii;
+
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld) *=scale;
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1) *= scale;
+
+		// int n4 = 2;
+		// int incxx = 1;
+		// blasf77_dscal(&n4,
+  //                     &scale,
+  //                     abftEnv->col_hchk + i * abftEnv->col_hchk_ld, &incxx );
 
 
-		printMatrix_host(A, lda, m, n, 4, 4);
 
-		cout << "column checksum" << endl;
-		printMatrix_host(abftEnv->col_hchk, abftEnv->col_hchk_ld, (m / abftEnv->chk_nb) * 2, n, 2, 4);
 
-		double AII = *(A + i * lda + i);
-		*(A + i * lda + i) = 1;
+		// printMatrix_host(A, lda, m, n, 4, 4);
+
+		// cout << "column checksum" << endl;
+		// printMatrix_host(abftEnv->col_hchk, abftEnv->col_hchk_ld, (m / abftEnv->chk_nb) * 2, n, 2, 4);
+
+		// double AII = *(A + i * lda + i);
+		// *(A + i * lda + i) = 1;
+
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld) += 1;
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1) += i + 1;
+
+
+		// char L = 'L';
+		// lapackf77_dlarf( &L, &m2, &n2,
+  //                        A + i * lda + i, &incx,
+  //                        tau + i,
+  //                        A + (i + 1) * lda + i, &lda,
+  //                        work );
 
 		char L = 'L';
 		lapackf77_dlarf( &L, &m2, &n2,
-                         A + i * lda + i, &incx,
+                         v, &incx,
                          tau + i,
-                         A + (i + 1) * lda + i, &lda,
+                         A + i * lda + i, &lda,
                          work );
-		*(A + i * lda + i) = AII;
 
-		printMatrix_host(A, lda, m, n, 4, 4);
+		// char T = 'T';
+		// int two = 2;
+		// int one = 1;
+		// blasf77_dgemv( &T,
+  //                    &m2, &two,
+  //                    &one,
+  //                    abftEnv->row_hchk, abftEnv->row_hchk_ld,
+  //                    const double *x, const magma_int_t *incx,
+  //                    const double *beta,
+  //                          double *y, const magma_int_t *incy );
+
+		// blasf77_dger(  const magma_int_t *m, const magma_int_t *n,
+  //                    const double *alpha,
+  //                    const double *x, const magma_int_t *incx,
+  //                    const double *y, const magma_int_t *incy,
+  //                          double *A, const magma_int_t *lda );
+
+		// blasf77_dger(  const magma_int_t *m, const magma_int_t *n,
+  //                    const double *alpha,
+  //                    const double *x, const magma_int_t *incx,
+  //                    const double *y, const magma_int_t *incy,
+  //                          double *A, const magma_int_t *lda );
+
+
+		// *(A + i * lda + i) = AII;
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld) -= 1;
+		// *(abftEnv->col_hchk + i * abftEnv->col_hchk_ld + 1) -= i + 1;
+
+
+
+		//printMatrix_host(A, lda, m, n, 4, 4);
 
 	}
 
