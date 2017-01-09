d480a636b0ebc063b89c48297668fd142a96e0d0
Jieyang Chen <cjy7117@gmail.com>
2017-01-08 17:23:29 -0700

    test

--- magma-1.6.2-autotuning/magmablas/dgeqrfFT.cpp
+++ magma-1.6.2-autotuning/magmablas/dgeqrfFT.cpp
@@ -109,7 +109,7 @@ void dgeqrfFT( int m, int n, double * A, int lda, double * tau, double * work, i
 		char T = 'T';
 		int two = 2;
 		int one = 1;
-		int d_zero = 0;
+		double d_zero = 0;
 		double * cw = new double[2];
 		int inccw = 1;
 		double neg_tau = *(tau + i) * -1;
